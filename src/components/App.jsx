f127bee063a96ef59e03fa78147e17fc7269c02e
OlhaBuhun <115622612+OlhaBuhun@users.noreply.github.com>
2023-12-18 19:43:46 +0200

    Created a ContactForm

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,45 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from "react";
+import { nanoid } from 'nanoid';
+import ContactForm from "./ContactForm/ContactForm";
+
+class App extends Component {
+  state = {
+    contacts: [],
+  }
+
+  createContact = (data) => {
+    const newContact = {
+      ...data,
+      id: nanoid()
+    }
+    console.log(newContact);
+  }
+
+  render() {
+    return (
+      <div
+        // style={{
+        //   height: '100vh',
+        //   display: 'flex',
+        //   justifyContent: 'center',
+        //   alignItems: 'center',
+        //   fontSize: 40,
+        //   color: '#010101'
+        // }}
+      >
+        <h1>Phonebook</h1>
+       <ContactForm createContact={this.createContact}/>
+      <div>
+          <h2>Contacts</h2>
+            <ul>
+
+            </ul>
+      </div>  
+       
+      </div>
+    );
+  };
+  }
+
+  export default App;
+  
---
+++ src/components/ContactForm/ContactForm.jsx
@@ -0,0 +1,51 @@
+import  { Component } from "react";
+
+class ContactForm extends Component {
+
+  state = {
+    name: '',
+    number: ''
+  }
+
+  handleChange = ({target}) => {
+    this.setState({
+      [target.name]:target.value
+    })
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault()
+
+    this.props.createContact({
+      name: this.state.name,
+      number: this.state.number
+    })
+  }
+
+  render() {
+    return(
+    <form onSubmit={this.handleSubmit}>
+      <label htmlFor="name">Name
+        <input 
+          type="text" 
+          name="name" 
+          onChange={this.handleChange} 
+          value={this.state.name} 
+          required />
+      </label>
+      <label htmlFor="name">Number
+        <input 
+          type="text" 
+          name="number"
+          onChange={this.handleChange} 
+          value={this.state.number} 
+          required />
+      </label>
+      <button type="submit" >Add contact</button>
+    </form>
+    )
+  }
+
+}
+
+export default ContactForm;
